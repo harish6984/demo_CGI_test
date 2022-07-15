e37222200d5dc8085e5c0c514ae2e075bf84b2c3
harish6984 <maddukuriharishchowdary@gmail.com>
2022-07-15 14:50:03 +0200

    --

---
+++ src/main/java/CGI_test/example/demo_CGI_test/DemoCgiTestApplication.java
@@ -0,0 +1,13 @@
+package CGI_test.example.demo_CGI_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCgiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCgiTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CGI_test/example/demo_CGI_test/DemoCgiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package CGI_test.example.demo_CGI_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCgiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
